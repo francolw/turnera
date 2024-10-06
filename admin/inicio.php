8e74588c4353d15a2a88559856c2e222ff41662c
francolw <lopezwagnerf@gmail.com>
2024-10-06 15:02:50 -0300

    #se implementa API de clima

--- admin/inicio.php
+++ admin/inicio.php
@@ -8,6 +8,7 @@
 require '../admin/header.php';
 require '../admin/navbar.php';
 require '../admin/model/conexion.php';
+require '../weatherAPI/apiData.php';
 
 $query = "SELECT
                 tt.id AS id,
@@ -35,18 +36,25 @@ $query = "SELECT
 $stmt = $db->prepare($query);
 $stmt->execute();
 $tabla_turnos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+$temperature = isset($forecast['DailyForecasts'][0]['Temperature']['Maximum']['Value']) ? $forecast['DailyForecasts'][0]['Temperature']['Maximum']['Value'] : 'N/A';
+$description = isset($forecast['DailyForecasts'][0]['Day']['IconPhrase']) ? $forecast['DailyForecasts'][0]['Day']['IconPhrase'] : 'N/A';
+$sensation = isset($forecast['DailyForecasts'][0]['RealFeelTemperature']['Maximum']['Value']) ? $forecast['DailyForecasts'][0]['RealFeelTemperature']['Maximum']['Value'] : 'N/A';
+$iconCode = isset($forecast['DailyForecasts'][0]['Day']['Icon']) ? str_pad($forecast['DailyForecasts'][0]['Day']['Icon'], 2, '0', STR_PAD_LEFT) : '01';
+
 ?>
 
 
 <div class="banner">
     <div class="weather-info">
-        <h3>Clima en Crespo</h3>
-        <h1 class="temp">18.75°C</h1>
-        <p class="description">Pocas nubes</p>
-        <p class="footer">Sensación térmica: 18.4°C</p>
+    <h3>Clima en Crespo</h3>
+        <h1 class="temp"><?php echo $temperature; ?>°C</h1>
+        <p class="description"><?php echo $description; ?></p>
+        <p class="footer">Sensación térmica: <?php echo $sensation; ?>°C</p>
     </div>
     <div>
-        <img src="https://openweathermap.org/img/wn/02n.png" alt="icono clima" class="icon">
+        <img src="https://developer.accuweather.com/sites/default/files/<?php echo $iconCode; ?>-s.png" alt="icono clima" class="icon">
     </div>
 </div>
 
